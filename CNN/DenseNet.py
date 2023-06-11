9a8d0848f7d267f3509c59759636b2953a8355d2
Myrrolinz <70125231+Myrrolinz@users.noreply.github.com>
2023-06-11 17:37:42 +0800

    update densenet
    too slow

--- CNN/DenseNet.py
+++ CNN/DenseNet.py
@@ -23,93 +23,77 @@ else:
 
 # DenseNet之中的Dense结构，每一个DenseBlock之中可能有几个DenseLayer
 # 加入了dropout层训练时防止过拟合
-class _DenseLayer(nn.Sequential):
-    def __init__(self, num_input_features, growth_rate, bn_size, drop_rate):
-        super(_DenseLayer, self).__init__()
-        # 这里是pre-activation的结构
-        self.add_module("norm1", nn.BatchNorm2d(num_input_features))
-        self.add_module("relu1", nn.ReLU(inplace=True))
-        self.add_module("conv1", nn.Conv2d(num_input_features, bn_size*growth_rate, kernel_size=1, stride=1, bias=False))
-        self.add_module("norm2", nn.BatchNorm2d(bn_size*growth_rate))
-        self.add_module("relu2", nn.ReLU(inplace=True))
-        self.add_module("conv2", nn.Conv2d(bn_size*growth_rate, growth_rate, kernel_size=3, stride=1, padding=1, bias=False))
-        self.drop_rate = drop_rate
-        self.add_module("dropout", nn.Dropout(drop_rate))
+class Bottleneck(nn.Module):
+    def __init__(self, nChannels, growthRate):
+        super(Bottleneck, self).__init__()
+        interChannels = 4*growthRate
+        self.bn1 = nn.BatchNorm2d(nChannels)
+        self.conv1 = nn.Conv2d(nChannels, interChannels, kernel_size=1,
+                               bias=False)
+        self.bn2 = nn.BatchNorm2d(interChannels)
+        self.conv2 = nn.Conv2d(interChannels, growthRate, kernel_size=3,
+                               padding=1, bias=False)
 
     def forward(self, x):
-        new_features = super(_DenseLayer, self).forward(x)
-        # 在通道维上将输入和输出连结
-        return torch.cat([x, new_features], 1)
+        out = self.conv1(F.relu(self.bn1(x)))
+        out = self.conv2(F.relu(self.bn2(out)))
+        out = torch.cat((x, out), 1)
+        return out
 
-# 这里就是DenseBlock结构，对于DenseNet之中应该都是加入了BottleNeck结构
-# 一般bn_size为4
-class _DenseBlock(nn.Sequential):
-    def __init__(self, num_layers, num_input_features, bn_size, growth_rate, drop_rate):
-        super(_DenseBlock, self).__init__()
-        for i in range(num_layers):
-            # i*growth_rate保证连接能够对的上维度
-            layer = _DenseLayer(num_input_features + i*growth_rate, growth_rate, bn_size, drop_rate)
-            self.add_module("denselayer%d" % (i+1), layer)
+class SingleLayer(nn.Module):
+    def __init__(self, nChannels, growthRate):
+        super(SingleLayer, self).__init__()
+        self.bn1 = nn.BatchNorm2d(nChannels)
+        self.conv1 = nn.Conv2d(nChannels, growthRate, kernel_size=3,
+                               padding=1, bias=False)
 
-# 不同的block之间可能会出现维度被我们调整的情形，这里就是采样降维转换层
-class _Transition(nn.Sequential):
-    def __init__(self, num_input_features, num_output_features):
-        super(_Transition, self).__init__()
-        self.add_module('norm', nn.BatchNorm2d(num_input_features))
-        self.add_module('relu', nn.ReLU(inplace=True))
-        self.add_module('conv', nn.Conv2d(num_input_features, num_output_features,
-                                          kernel_size=1, stride=1, bias=False))
-        self.add_module('pool', nn.AvgPool2d(kernel_size=2, stride=2))
+    def forward(self, x):
+        out = self.conv1(F.relu(self.bn1(x)))
+        out = torch.cat((x, out), 1)
+        return out
 
-# 最后就是完成对DenseNet结构的定义，包含了Transition的压缩部分(DenseNet-C)
-# 也包含了(DenseNet-B)也就是BottleNeck结构，这里给出的是DenseNet-121网络
+class Transition(nn.Module):
+    def __init__(self, nChannels, nOutChannels):
+        super(Transition, self).__init__()
+        self.bn1 = nn.BatchNorm2d(nChannels)
+        self.conv1 = nn.Conv2d(nChannels, nOutChannels, kernel_size=1,
+                               bias=False)
+
+    def forward(self, x):
+        out = self.conv1(F.relu(self.bn1(x)))
+        out = F.avg_pool2d(out, 2)
+        return out
+
+
 class DenseNet(nn.Module):
-    """
-    :param growth_rate: 增长率，即K=32
-    :param block_config: 每一个DenseBlock的layers数量，这里实现的是DenseNet-121
-    :param num_init_features: 第一个卷积的通道数一般为64
-    :param bn_size: bottleneck中1*1conv的factor=4，1*1conv输出的通道数一般为factor*K=128
-    :param compression_rate: 压缩因子
-    :param drop_rate: dropout层将神经元置0的概率，为0时表示不使用dropout层
-    :param num_classes: 分类数
-    Args:
-        growth_rate (int) - how many filters to add each layer (`k` in paper)
-        block_config (list of 4 ints) - how many layers in each pooling block
-        num_init_features (int) - the number of filters to learn in the first convolution layer
-        bn_size (int) - multiplicative factor for number of bottle neck layers
-        (i.e. bn_size * k features in the bottleneck layer)
-        drop_rate (float) - dropout rate after each dense layer
-        num_classes (int) - number of classification classes
-    """
-    def __init__(self, growth_rate=12, block_config=(16, 16, 16),
-                 num_init_features=24, bn_size=4, drop_rate=0, num_classes=10):
-
+    def __init__(self, growthRate, depth, reduction, nClasses, bottleneck):
         super(DenseNet, self).__init__()
 
-        # First convolution
-        self.features = nn.Sequential(OrderedDict([
-            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=3, stride=1, padding=1, bias=False)),
-        ]))
+        nDenseBlocks = (depth-4) // 3
+        if bottleneck:
+            nDenseBlocks //= 2
 
-        # Each denseblock
-        num_features = num_init_features
-        for i, num_layers in enumerate(block_config):
-            block = _DenseBlock(num_layers=num_layers, num_input_features=num_features,
-                                bn_size=bn_size, growth_rate=growth_rate, drop_rate=drop_rate)
-            self.features.add_module('denseblock%d' % (i + 1), block)
-            num_features = num_features + num_layers * growth_rate
-            if i != len(block_config) - 1:
-                trans = _Transition(num_input_features=num_features, num_output_features=num_features // 2)
-                self.features.add_module('transition%d' % (i + 1), trans)
-                num_features = num_features // 2
+        nChannels = 2*growthRate
+        self.conv1 = nn.Conv2d(3, nChannels, kernel_size=3, padding=1,
+                               bias=False)
+        self.dense1 = self._make_dense(nChannels, growthRate, nDenseBlocks, bottleneck)
+        nChannels += nDenseBlocks*growthRate
+        nOutChannels = int(math.floor(nChannels*reduction))
+        self.trans1 = Transition(nChannels, nOutChannels)
 
-        # Final batch norm
-        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
+        nChannels = nOutChannels
+        self.dense2 = self._make_dense(nChannels, growthRate, nDenseBlocks, bottleneck)
+        nChannels += nDenseBlocks*growthRate
+        nOutChannels = int(math.floor(nChannels*reduction))
+        self.trans2 = Transition(nChannels, nOutChannels)
 
-        # Linear layer
-        self.classifier = nn.Linear(num_features, num_classes)
-        
-        # initialize conv and bn parameters
+        nChannels = nOutChannels
+        self.dense3 = self._make_dense(nChannels, growthRate, nDenseBlocks, bottleneck)
+        nChannels += nDenseBlocks*growthRate
+
+        self.bn1 = nn.BatchNorm2d(nChannels)
+        self.fc = nn.Linear(nChannels, nClasses)
+
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
@@ -117,23 +101,38 @@ class DenseNet(nn.Module):
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.bias.data.zero_()
+
+    def _make_dense(self, nChannels, growthRate, nDenseBlocks, bottleneck):
+        layers = []
+        for i in range(int(nDenseBlocks)):
+            if bottleneck:
+                layers.append(Bottleneck(nChannels, growthRate))
+            else:
+                layers.append(SingleLayer(nChannels, growthRate))
+            nChannels += growthRate
+        return nn.Sequential(*layers)
 
     def forward(self, x):
-        features = self.features(x)
-        out = F.relu(features, inplace=True)
-        out = F.avg_pool2d(out, kernel_size=8, stride=1).view(features.size(0), -1)
-        out = self.classifier(out)
+        out = self.conv1(x)
+        out = self.trans1(self.dense1(out))
+        out = self.trans2(self.dense2(out))
+        out = self.dense3(out)
+        out = torch.squeeze(F.avg_pool2d(F.relu(self.bn1(out)), 8))
+        out = F.log_softmax(self.fc(out))
         return out
 
 
-def densenet_BC_cifar(depth, k, **kwargs):
-    N = (depth - 4) // 6
-    model = DenseNet(growth_rate=k, block_config=[N, N, N], num_init_features=2*k, **kwargs)
-    return model
+# def densenet_BC_cifar(depth, k, **kwargs):
+#     N = (depth - 4) // 6
+#     model = DenseNet(growth_rate=k, block_config=[N, N, N], num_init_features=2*k, **kwargs)
+#     return model
 
 # 直接设置好默认的分类类别有10类
 # net = DenseNet().to(device)
-net = densenet_BC_cifar(190, 40, num_classes=100).to(device)
+# net = densenet_BC_cifar(190, 40, num_classes=100).to(device)
+net = DenseNet(growthRate=12, depth=100, reduction=0.5, bottleneck=True, nClasses=10).to(device)
 with open('Log/DenseNet.txt', 'w') as f:
     f.write(str(net))
 criterion = nn.CrossEntropyLoss()
@@ -190,9 +189,9 @@ if __name__ == '__main__':
     # 设置好对应读取数据集和测试集的loader
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     batch_size = 4
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
 
     print('Using PyTorch version:', torch.__version__, ' Device:', device)
